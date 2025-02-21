fb20842c80c1de82e96e7937ef354e38b760b99f
AditiShetti <144339282+AditiShetti@users.noreply.github.com>
2025-02-21 20:48:32 +0530

    Add files via upload

---
+++ Java Practice/Main.java
@@ -0,0 +1,25 @@
+//Check if a number is Prime or not
+//Prime no within a range
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+//    System.out.print("Enter a number to check if prime or not n:");
+//    int n= in.nextInt();
+//    int flag=0;
+//    for (int i=2;i<n;i++)
+//        if (n%i==0) {
+//            flag = 1;
+//            break;
+//        }
+//
+//    if (flag==0)
+//        System.out.print(n+" is a prime number");
+//    else
+//        System.out.print(n+" is not a prime number");
+
+        //Prime nos within a range
+
+
+    }
+}
