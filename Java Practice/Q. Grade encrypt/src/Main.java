3d9968d2d442302aba68f392ac45a557f306c2d5
Aditi Shetti <144339282+AditiShetti@users.noreply.github.com>
2025-03-09 23:52:53 +0530

    Update Main.java

--- Java Practice/Q. Grade encrypt/src/Main.java
+++ Java Practice/Q. Grade encrypt/src/Main.java
@@ -1,7 +1,7 @@
 //Grade encryption and decryption
 public class Main {
     public static void main(String[] args) {
-    //While sending char encrypt by adding value by 8. then decrypt itlater .
+    //While sending char encrypt by adding value by 8. then decrypt it later .
     char grade='B';
     grade= (char) (grade +8);
     System.out.println(grade);
